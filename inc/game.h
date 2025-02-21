7656f84df52e5569937c1d5f9ff69326a106a164
Tamar Feuer <tfeuer@f1r6s11.codam.nl>
2025-02-21 15:48:00 +0100

    grid is resizing well

--- inc/game.h
+++ inc/game.h
@@ -171,7 +171,7 @@ typedef struct s_game
 	bool			is_mmap;
 	bool			is_mouse_active;
 	t_mmap			mmap;
-	int				tile_size;
+	int				cell_size;
 	mlx_t			*mlx;
 	char			*mapdata;
 	mlx_image_t 	*scene;
