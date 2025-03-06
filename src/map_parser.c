74edb74913cab5ad1d9445903f1aedb7abac2c39
Roger Torrent <rtorrent@student.codam.nl>
2025-03-06 15:59:57 +0100

    All tests passed!

--- src/map_parser.c
+++ src/map_parser.c
@@ -6,7 +6,7 @@
 /*   By: rtorrent <marvin@42.fr>                       +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2025/02/24 13:12:32 by rtorrent       #+#    #+#                */
-/*   Updated: 2025/03/06 14:27:28 by rtorrent       ########   odam.nl        */
+/*   Updated: 2025/03/06 14:49:13 by rtorrent       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,7 @@ static void	determine_dimensions(char *log, t_data *data,
 		token = map_tokens->content;
 		if (token->line > last_line + 1)
 			return ((void)ft_snprintf(log, LOG, "Empty map row detected at "
-					"line %d", ++last_line));
+					"line %d", last_line + 2));
 		if (token->line > last_line)
 		{
 			last_line++;
