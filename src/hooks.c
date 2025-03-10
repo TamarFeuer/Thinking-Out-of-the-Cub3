234a2422987880d74c36bef569d2c600cabd5c12
Tamar Feuer <tamar2603@gmail.com>
2025-03-10 16:30:45 +0100

    what's up with the diagonal collision?!

--- src/hooks.c
+++ src/hooks.c
@@ -1,5 +1,66 @@
 #include "../inc/game.h"
 
+bool is_diagonal_collision(t_game *game, t_pos new)
+{
+    t_pos new_tl = new;
+	t_pos new_tr = {new.x + PLAYER_SIZE * CONST, new.y};
+	t_pos new_bl = {new.x, new.y + PLAYER_SIZE * CONST};
+	t_pos new_br = {new.x + PLAYER_SIZE * CONST, new.y + PLAYER_SIZE * CONST};
+	t_pos check1;
+	t_pos check2;
+
+	// Check four diagonal corners
+	if (game->player.angle_quad == 1)
+	{
+		// Top right of player
+		check1.x = round(new_tr.x / CONST);
+		check1.y = round((new_tr.y - 1) / CONST);
+		check2.x = round((new_tr.x - 1) / CONST);
+		check2.y = round(new_tr.y / CONST);
+	}
+	else if (game->player.angle_quad == 2)
+	{
+		// Top left of player
+		check1.x = round(new_tl.x / CONST);
+		check1.y = round((new_tl.y - 1) / CONST);
+		check2.x = round((new_tl.x + 1) / CONST);
+		check2.y = round(new_tl.y / CONST);
+	}
+	else if (game->player.angle_quad == 3)
+	{
+		// Bottom left of player
+		check1.x = round(new_bl.x / CONST);
+		check1.y = round((new_bl.y + 1) / CONST);
+		check2.x = round((new_bl.x - 1) / CONST);
+		check2.y = round(new_bl.y / CONST);
+	}
+	else if (game->player.angle_quad == 4)
+	{
+		// Bottom right of player
+		check1.x = round(new_br.x / CONST);
+		check1.y = round((new_br.y + 1) / CONST);
+		check2.x = round((new_br.x + 1) / CONST);
+		check2.y = round(new_br.y / CONST);
+	}
+
+	printf("check1 is x=%f, y=%f\n", check1.x, check1.y);
+	printf("check2 is x=%f, y=%f\n", check2.x, check2.y);
+
+	int index_horiz1 = get_block_index2(game, &check1, 0);
+	int index_vert1 = get_block_index2(game, &check1, 1);
+	int index_horiz2 = get_block_index2(game, &check2, 0);
+	int index_vert2 = get_block_index2(game, &check2, 1);
+
+	if ((game->data->map[index_horiz1] == '1' && game->data->map[index_vert1] == '1')
+		&& (game->data->map[index_horiz2] == '1' && game->data->map[index_vert2] == '1'))
+	{
+		printf("Diagonal collision!\n");
+		return true;
+	}
+	return false;
+}
+
+
 bool is_horiz_collision(t_game *game, t_pos new)
 {
 	t_pos	new_tl = new;
@@ -41,6 +102,35 @@ bool is_vertical_collision(t_game *game, t_pos new)
 }
 
 
+void check_collision(t_game *game, t_pos *new_pos) 
+{
+    // Check for collisions in all directions
+    bool horiz_collision = is_horiz_collision(game, *new_pos);  // Check horizontal collision
+    bool vert_collision = is_vertical_collision(game, *new_pos);  // Check vertical collision
+    bool diag_collision = is_diagonal_collision(game, *new_pos);  // Check diagonal collision
+
+    // Handle movement based on the type of collision
+    if (!diag_collision) 
+	{
+
+        // If there's no horizontal collision, the player can move vertically (up or down)
+        if (!horiz_collision) 
+		{
+            game->player.p_pos.y = round(new_pos->y);  // Move player vertically
+        }
+
+        // If there's no vertical collision, the player can move horizontally (left or right)
+        if (!vert_collision) 
+		{
+            game->player.p_pos.x = round(new_pos->x);  // Move player horizontally
+        }
+    }
+
+    // Update the camera position based on the player's new position
+    game->camera.pos.x = round(game->player.p_pos.x + (PLAYER_SIZE + CONST) / 2);
+    game->camera.pos.y = round(game->player.p_pos.y + (PLAYER_SIZE + CONST) / 2);
+}
+
 static void check_keys_for_movement(t_game *game, mlx_key_data_t keydata)
 {
 	t_pos new;
@@ -80,20 +170,7 @@ static void check_keys_for_movement(t_game *game, mlx_key_data_t keydata)
 			new.x = X_START + game->data->minimap_data.width - CONST;
 		if (new.y > Y_START + game->data->minimap_data.height - CONST)
 			new.y = Y_START + game->data->minimap_data.height - CONST;
-	
-		if (!is_horiz_collision(game, new))
-		{
-			game->player.p_pos.y = round(new.y);
-			//game->camera.pos.y = round(game->player.p_pos.y + CONST /2);
-			game->camera.pos.y = round(game->player.p_pos.y + (PLAYER_SIZE + CONST) /2);
-		}
-		
-		if (!is_vertical_collision(game, new))
-		{
-			game->player.p_pos.x = round(new.x);
-			//game->camera.pos.x = round(game->player.p_pos.x + CONST /2);
-			game->camera.pos.x = round(game->player.p_pos.x + (PLAYER_SIZE + CONST) /2);
-		}
+		check_collision(game, &new);
 	}
 	if (game->is_mouse_active == false &&(keydata.key == MLX_KEY_RIGHT && (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT)))
 		new_angle -= angle_size; // Rotate clockwise (right)
